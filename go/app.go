15bcb3820ac4a719f8f9fdb7982bc22da6925912
satellitex <s.a.t.e.3.ths@gmail.com>
2017-10-18 02:30:31 +0900

    add render

--- go/app.go
+++ go/app.go
@@ -122,7 +122,7 @@ func GenTicketHTML(r *Render) string {
 	return ""
 }
 
-func GenHTML(content_name string) string {
+func GenHTML(content_name string, r *Render) string {
 	res := ` <!DOCTYPE html> <html> <head>	<title>isucon 2</title>	<meta charset="utf-8">	<link type="text/css" rel="stylesheet" href="/css/ui-lightness/jquery-ui-1.8.24.custom.css">	<link type="text/css" rel="stylesheet" href="/css/isucon2.css">	<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>	<type="text/javascript" src="/js/jquery-ui-1.8.24.custom.min.js"></script>	<script type="text/javascript" src="/js/isucon2.js"></script>	</head>	<body>	<header>	<a href="/">	<img src="/images/isucon_title.jpg">	</a>	</header>	<div id="sidebar">`
 	if !emptySold {
 		res += `<table>
